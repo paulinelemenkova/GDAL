34613b7b4b72b05c1278307e936b0bfb3cd49aa0
Pauline Lemenkova <pauline.lemenkova@gmail.com>
2020-12-04 13:17:06 +0300

    Add files via upload

--- 01_GDAL_gdalwarp.sh
+++ 01_GDAL_gdalwarp.sh
@@ -25,3 +25,18 @@ gdalwarp -t_srs '+proj=lcc +lon_0=155 +lat_1=45 +lat_2=55 +datum=WGS8' ETOPO1_KK
 
 # stored in GeoTIFF in WGS84 warped to a Two Point Equidistant Azimuthal projection:
 gdalwarp -t_srs '+proj=tpeqd +lat_1=45 +lat_2=55 +lon_1=100 +lon_2=120' ETOPO1_KKT_WGS84.tif ETOPO1_KKT_2p.tif -overwrite
+
+# stored in GeoTIFF in WGS84 warped to a Two Point Equidistant Azimuthal projection:
+gdalwarp -t_srs '+proj=ups +south +datum=WGS84' ant_sed_wgs84.nc ant_sed_ups.nc -overwrite
+
+# gdalwarp code to convert GeoTIFF file from XY Cartesian to WGS84, from NetCDF to GeoTIFF
+gdalwarp -t_srs EPSG:4326 ant_sed.nc ant_sed_wgs84.tif
+
+# check up the projection
+gdalinfo ant_sed_wgs84.tif
+
+# re-projecting to UPS projection (Universal Polar Stereographic)
+gdalwarp -t_srs '+proj=ups +south +datum=WGS84' ant_sed_wgs84.tif ant_sed_ups.tif -overwrite
+
+# check up the projection
+gdalinfo ant_sed_ups.tif
